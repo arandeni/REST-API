2886fc9ea3059cb5e332bef49181b07484c43c80
Achini Randeni <achinirandeni@gmail.com>
2024-08-08 11:41:09 +0800

    added chnages

--- src/REST_API/Controllers/Employee2Controller.cs
+++ src/REST_API/Controllers/Employee2Controller.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using REST_API.Models;
 
 namespace REST_API.Controllers
 {
@@ -15,10 +16,52 @@ namespace REST_API.Controllers
         }
 
         //[Route("Emp/ById/{Id}")]
+        [Route("Employee/{Id}")]
         [HttpGet]
         public string GetEmployeeById(int id) 
         {
             return "Response from GetEmployeeById method";
+        }
+
+        [Route("Employee/{gender}/{city}")]
+        [HttpGet]
+        public string GetEmployeeByGenderAndCity(string gender, string city) 
+        {
+            return "Response from GetEmployeeByGenderAndCity method";
+        }
+
+        [Route("Employee/Search1")]
+        [HttpGet]
+        public string SearchEmployees(string department) 
+        {
+            return $"Return employees with department: {department}";
+        }
+
+        [Route("Employee/Search2")]
+        [HttpGet]
+        public string SearchEmployees(string? department, string? gender, string? city)
+        {
+            return $"Return employees with department: {department}, gender: {gender}, city: {city}";
+        }
+
+        [Route("Employee/Search3")]
+        [HttpGet]
+        public string SearchEmployees([FromQuery]EmployeeSearch search)
+        {
+            return $"Return employees with department: {search.department}, " +
+                $"gender: {search.gender}, city: {search.city}";
+        }
+
+        [Route("Employee/Search")]
+        [HttpGet]
+        public string SearchEmployees()
+        {
+            var department = HttpContext.Request.Query["department"].ToString();
+            var gender = HttpContext.Request.Query["gender"].ToString();
+            var city = HttpContext.Request.Query["city"].ToString();
+
+            return $"Return employees with department: {department}, " +
+                $"gender: {gender}, city: {city}";
         }
     }
 }
---
+++ src/REST_API/Models/EmployeeSearch.cs
@@ -0,0 +1,9 @@
+﻿namespace REST_API.Models
+{
+    public class EmployeeSearch
+    {
+        public string? department {  get; set; }
+        public string? gender { get; set; }
+        public string? city { get; set; }
+    }
+}
